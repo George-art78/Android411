dfc00108281b32382a3ac0dee581b450b9c96a8a
George Remizov <g.remizov78@gmail.com>
2024-11-23 21:09:00 +0300

    closer to the solution

--- src/HomeWorks/HomeWork15.java
+++ src/HomeWorks/HomeWork15.java
@@ -4,19 +4,22 @@ public class HomeWork15 {
     public static void main(String[] args) {
         String s = "Hello, World";
         System.out.println(s);
-        int[] result = new int[('Z' - 'A') + 1];
+        int[] resultLow = new int['z' - 'a' + 1];
+        int[] resultUp = new int['Z' - 'A' + 1];
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (ch >= 'Z' && ch <= 'A') {
-                result[ch - 'A']++;
+            if (ch >= 'A' && ch <= 'Z') {
+                resultUp[ch - 'A']++;
             }
             if (ch >= 'a' && ch <= 'z') {
-                result[ch - 'a']++;
+                resultLow[ch - 'a']++;
             }
         }
-        for (int i = 0; i < result.length; i++) {
-            System.out.println((char) (i + 'a') + " = " + result[i]);
-            System.out.println((char) (i + 'A') + " = " + result[i]);
+        for (int i = 0; i < resultLow.length; i++) {
+            System.out.println((char) (i + 'a') + " = " + resultLow[i]);
+        }
+        for (int i = 0; i < resultUp.length; i++) {
+            System.out.println((char) (i + 'A') + " = " + resultUp[i]);
         }
     }
 }
