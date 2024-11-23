e35e39685dd1beef87e71a49a01a9c78687e9b99
George Remizov <g.remizov78@gmail.com>
2024-11-23 22:22:18 +0300

    Done

--- src/HomeWorks/HomeWork15.java
+++ src/HomeWorks/HomeWork15.java
@@ -3,23 +3,23 @@ package HomeWorks;
 public class HomeWork15 {
     public static void main(String[] args) {
         String s = "Hello, World";
-        System.out.println(s);
         int[] resultLow = new int['z' - 'a' + 1];
         int[] resultUp = new int['Z' - 'A' + 1];
+        int countLow = 0;
+        int countUp = 0;
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (ch >= 'A' && ch <= 'Z') {
                 resultUp[ch - 'A']++;
+                countUp++;
             }
             if (ch >= 'a' && ch <= 'z') {
                 resultLow[ch - 'a']++;
+                countLow++;
             }
         }
-        for (int i = 0; i < resultLow.length; i++) {
-            System.out.println((char) (i + 'a') + " = " + resultLow[i]);
-        }
-        for (int i = 0; i < resultUp.length; i++) {
-            System.out.println((char) (i + 'A') + " = " + resultUp[i]);
-        }
+
+        System.out.println("Дана строка - " + s);
+        System.out.println("Количество строчных букв - " + countLow + ", количество прописных букв - " + countUp);
     }
 }
