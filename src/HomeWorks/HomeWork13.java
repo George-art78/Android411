398bcfed2f23ce9e704a9d01900c300635c52446
George Remizov <g.remizov78@gmail.com>
2024-11-23 10:20:40 +0300

    HomeWork13

--- src/HomeWorks/HomeWork13.java
+++ src/HomeWorks/HomeWork13.java
@@ -9,16 +9,12 @@ public class HomeWork13 {
 
         int[] arr = {-2, 3, 8, -11, -4, 6};
         System.out.println(Arrays.toString(arr));
-        System.out.print("n = " + negative(arr));
+        System.out.print("n = " + negative(arr, 0));
     }
 
-    static int negative(int[] array) {
-        int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
-                count++;
-            }
-        }
-        return 0;
+    static int negative(int[] array, int index) {
+        if (index == array.length)
+            return 0;
+        return (array[index] < 0 ? 1 : 0) + negative(array, index + 1);
     }
 }
