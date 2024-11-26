d44abd1cb44fbc8f3c0f99de2e85f82582281a3c
George Remizov <g.remizov78@gmail.com>
2024-11-26 19:35:43 +0300

    home work

---
+++ src/HomeWorks/HomeWork16.java
@@ -0,0 +1,29 @@
+package HomeWorks;
+
+import java.util.Scanner;
+
+public class HomeWork16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите фразу для проверки на палиндром: ");
+        String str = input.nextLine();
+        boolean result = palindrom(str);
+
+        if (result == true) {
+            System.out.println("Фраза является палиндромом.");
+        } else {
+            System.out.println("Фраза не является палиндромом.");
+        }
+    }
+    public static boolean palindrom(String s) {
+        String line = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            line += s.charAt(i);
+        }
+        if (line.replace(" ", "").equalsIgnoreCase(s.replace(" ", ""))) {
+            return true;
+        }
+        else return false;
+    }
+}
