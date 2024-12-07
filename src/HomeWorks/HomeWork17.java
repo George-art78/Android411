f30e94c210a25a024c50e774a607b397af30ab43
George Remizov <g.remizov78@gmail.com>
2024-12-07 21:04:33 +0300

    final

--- src/HomeWorks/HomeWork17.java
+++ src/HomeWorks/HomeWork17.java
@@ -3,16 +3,24 @@ package HomeWorks;
 
 public class HomeWork17 {
     public static void main(String[] args) {
-        StringBuilder orig = new StringBuilder(" Текст с повторящимися символами ");
+        String orig = " Текст с повторяющимися символами ";
+        StringBuilder builder = new StringBuilder();
         System.out.println("Исходная строка - \"" + orig + "\"");
-        modifyStr(orig, " ", "");
-        System.out.println("Изменённая строка - \"" + orig + "\"");
-    }
-
-    public static void modifyStr(StringBuilder str, String oldS, String newS) {
-        int position;
-        while ((position = str.indexOf(oldS)) != -1) {
-            str.replace(position, position + 1, newS);
+        for (int i = 0; i < orig.length(); i++) {
+            if (builder.indexOf(String.valueOf(orig.charAt(i))) == -1) {
+                builder.append(orig.charAt(i));
+            }
         }
+        int position;
+        if ((position = builder.indexOf(" ")) != -1) {
+            System.out.println("Изменённая строка - \"" + modifyStr(builder, " ", "") + "\"");
+        }
+    }
+    public static StringBuilder modifyStr(StringBuilder stBuild, String oldStr, String newStr) {
+        int pos;
+        while ((pos = stBuild.indexOf(oldStr)) != -1) {
+            stBuild.replace(pos, pos + 1, newStr);
+        }
+        return stBuild;
     }
 }
--- src/lessons7/Main.java
+++ src/lessons7/Main.java
@@ -1,22 +1,65 @@
 package lessons7;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "Я ищу совпадения в 2024. И я их найду в 2 счёта. 5910. Hel_lo. Ели[-ели]";
+//        String s = "Я ищу совпадения в 2024. И я их найду в 2 счёта. 5910. Hel_lo World. Ели[-ели]";
+//        String s = "one\ntwo";
+//        String s = "Java, \njava, \nJAVA";
+//        String s = "<body>Пример жадного соответствия регулярных выражений</body>";
 //        String pattern = "[12][0-9][0-9][0-9]";
 //        String pattern = "[А-яЁё]";
 //        String pattern = "[А-яЁё\\[\\]-]";
-        String pattern = "[^0-9]"; // поиск всего, что не входит в указанный диапазон
-        Pattern regex = Pattern.compile(pattern);
-        Matcher matcher = regex.matcher(s);
-//        System.out.println(matcher.find());
-        while (matcher.find()) {
-//            System.out.println("Шаблон совпадения найден с " + matcher.start() + " по " + (matcher.end() - 1));
-//            System.out.println(s.substring(matcher.start(), matcher.end()));
-            System.out.print(matcher.group());
-        }
+//        String pattern = "\\d+ #поиск цифр"; // поиск всего, что не входит в указанный диапазон
+//        String pattern = "one.\\w+";
+//        String pattern = "one$";
+//        String pattern = "(?im)^java";
+//        String s = "Егорова Алиса Александровна";
+//        String s = "Word2016, PS6, AI5";
+//        String s = "one";
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Введите дату в формате dd-mm-YYYY: ");
+////        String s = input.nextLine();
+//////        String pattern = "([a-z]+)(\\d*)";
+//////        String pattern = "(.(.(.)))";
+//////        String pattern = "Петр|Ольга|Виталий";
+//////        String pattern = "\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}";
+//////        String pattern = "(\\d{1,3}.){3}\\d{1,3}";
+////        String pattern = "(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(19[0-9][0-9]|20[0-9][0-9])";
+//////        String pattern = "A.+a";
+//////        Pattern regex = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE); // поиск загравных букв + игнор языка
+//////        Pattern regex = Pattern.compile(pattern, Pattern.DOTALL);
+//////        Pattern regex = Pattern.compile(pattern, Pattern.MULTILINE);
+////        Pattern regex = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+////        Matcher matcher = regex.matcher(s);
+//////        System.out.println(matcher.find());
+////        while (matcher.find()) {
+////////            System.out.println("Шаблон совпадения найден с " + matcher.start() + " по " + (matcher.end() - 1));
+////////            System.out.println(s.substring(matcher.start(), matcher.end()));
+////            System.out.println(matcher.group());
+////        }
+
+//        matcher.find();
+//        System.out.println(matcher.groupCount());
+//        for (int i = 0; i <= matcher.groupCount(); i++) {
+//            System.out.println(i + ": " + matcher.group(i));
+//        }
+
+//        String s = "Я ищу совпадения в 2024. И я их найду в 2 счёта. 5910. Hel_lo World. Ели[-ели].";
+//        String s = "Суворова Виктория, Россия, г. Сочи, +7 999 555-77-33";
+        String s = "05-03-1987 // Дата рождения";
+        String s2 = s.replaceAll("\\s//.*", "").replaceAll("-", ".");
+        System.out.println("Дата рождения: " + s2);
+//        String[] arr = s.split("\\.");
+//        String[] arr = s.split(",\\s");
+//        for (String temp : arr) {
+//            System.out.println(temp);
+//        }
+
+
     }
 }
