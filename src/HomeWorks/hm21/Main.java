b00a453481a2f74b3c9931d564fae1143c2042ca
George Remizov <g.remizov78@gmail.com>
2024-12-18 05:25:05 +0300

    squares

--- src/HomeWorks/hm21/Main.java
+++ src/HomeWorks/hm21/Main.java
@@ -2,6 +2,9 @@ package HomeWorks.hm21;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Площадь квадрата: " + Square.square(7));
+        System.out.println("Площадь треугольника по формуле Герона: " + Square.triangleGerone(3, 4, 5));
+        System.out.println("Площадь треугольника через основание и высоту: " + Square.triangleBaseAndHeight(6, 7));
+        System.out.println("Площадь прямоугольника: " + Square.rectangle(2, 6));
     }
 }
--- src/HomeWorks/hm21/Square.java
+++ src/HomeWorks/hm21/Square.java
@@ -1,4 +1,22 @@
 package HomeWorks.hm21;
 
 public class Square {
+    private static int count;
+
+    public static int square(int a) {
+       return (int) Math.pow(a, 2);
+    }
+
+    public static double triangleGerone (int a, int b, int c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public static double triangleBaseAndHeight(double b, double h) {
+        return (b * h) / 2;
+    }
+
+    public static int rectangle (int a, int b) {
+        return a * b;
+    }
 }
