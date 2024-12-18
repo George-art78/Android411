fee6b4f18f70cd95376dbd9ab98799acec60db64
George Remizov <g.remizov78@gmail.com>
2024-12-18 06:26:02 +0300

    solution

--- src/HomeWorks/hm21/Main.java
+++ src/HomeWorks/hm21/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Площадь треугольника по формуле Герона: " + Square.triangleGerone(3, 4, 5));
         System.out.println("Площадь треугольника через основание и высоту: " + Square.triangleBaseAndHeight(6, 7));
         System.out.println("Площадь прямоугольника: " + Square.rectangle(2, 6));
+        System.out.println("Количество подсчётов площади: " + Square.getCount());
     }
 }
--- src/HomeWorks/hm21/Square.java
+++ src/HomeWorks/hm21/Square.java
@@ -1,22 +1,30 @@
 package HomeWorks.hm21;
 
 public class Square {
-    private static int count;
+    static int count;
 
     public static int square(int a) {
+        count++;
        return (int) Math.pow(a, 2);
     }
 
     public static double triangleGerone (int a, int b, int c) {
         double p = (a + b + c) / 2;
+        count++;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static double triangleBaseAndHeight(double b, double h) {
+        count++;
         return (b * h) / 2;
     }
 
     public static int rectangle (int a, int b) {
+        count++;
         return a * b;
+    }
+
+    public static int getCount() {
+        return count;
     }
 }
