10e19e87e224249f58a48e2d5ae20aec59dc9cd5
George Remizov <g.remizov78@gmail.com>
2024-12-26 20:57:57 +0300

    finish

--- src/HomeWorks/hm22/Main.java
+++ src/HomeWorks/hm22/Main.java
@@ -5,5 +5,17 @@ public class Main {
         Box3D box = new Box3D(5, 3, 2);
         System.out.println(box);
         System.out.println("Объём: " + (box.getDeep() * box.getHeight()* box.getWidth()));
+        System.out.println();
+
+        box.setWidth(10);
+        box.setHeight(5);
+        box.setDeep(7);
+        System.out.println("Новые значения");
+        System.out.println("Ширина: " + box.getWidth());
+        System.out.println("Высота: " + box.getHeight());
+        System.out.println("Глубина: " + box.getDeep());
+
+        System.out.println(box);
+        System.out.println("Объём: " + (box.getDeep() * box.getWidth() * box.getHeight()));
     }
 }
