2656f485027d887705915c5a427fd07dec77202a
George Remizov <g.remizov78@gmail.com>
2025-01-03 04:28:47 +0300

    finish

--- src/HomeWorks/hm24/Main.java
+++ src/HomeWorks/hm24/Main.java
@@ -2,6 +2,13 @@ package HomeWorks.hm24;
 
 public class Main {
     public static void main(String[] args) {
+        Figure3d[] figures = new Figure3d[3];
+        figures[0] = new Ball(5);
+        figures[1] = new Cylinder(9, 15);
+        figures[2] = new Pyramid(17, 125);
 
+        for (int i = 0; i < figures.length; i++) {
+            figures[i].calcVolume();
+        }
     }
 }
