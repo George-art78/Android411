157f231c76611c352c9aa859980d86e907473d4b
George Remizov <g.remizov78@gmail.com>
2025-01-21 15:28:48 +0300

    karkas

---
+++ src/HomeWorks/hm28/HaveNotVowelLettersException.java
@@ -0,0 +1,7 @@
+package HomeWorks.hm28;
+
+public class HaveNotVowelLettersException extends Exception{
+    public HaveNotVowelLettersException(String message) {
+        super(message);
+    }
+}
---
+++ src/HomeWorks/hm28/VowelLetters.java
@@ -0,0 +1,22 @@
+package HomeWorks.hm28;
+
+import java.util.Scanner;
+
+public class VowelLetters {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String s = input.nextLine();
+
+    }
+
+    public static void voLetters(String s) {
+        String vowelLetters = "eyuioaуеаоэяию";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < vowelLetters.length(); j++) {
+                
+            }
+        }
+    }
+}
