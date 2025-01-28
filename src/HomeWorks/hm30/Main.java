5e10caa44c2364b9e211427c480f43217ae0cd55
George Remizov <g.remizov78@gmail.com>
2025-01-28 04:44:24 +0300

    hm30

--- src/HomeWorks/hm30/Main.java
+++ src/HomeWorks/hm30/Main.java
@@ -1,7 +1,21 @@
 package HomeWorks.hm30;
 
+import java.util.Scanner;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        Predicate<String> line = str -> str.isEmpty();
+
+        System.out.print("Строка 1: ");
+        String s1 = input.nextLine();
+        boolean res = line.test(s1);
+        System.out.println("Строка 1 пустая: " + res);
+
+        System.out.print("Строка 2: ");
+        String s2 = input.nextLine();
+        boolean res2 = line.test(s2);
+        System.out.println("Строка 2 пустая: " + res2);
     }
 }
